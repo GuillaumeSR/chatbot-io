958be240187e0cdc2556aea338f7214e88ce4e6c
Maxime <maxime.lecourt457@gmail.com>
2024-02-16 11:16:44 +0100

    descriptionbot

--- src/views/bots.js
+++ src/views/bots.js
@@ -5,28 +5,28 @@ export default () => (
         <img src="https://katana-anime.com/cdn/shop/articles/727e572c1a4503e860f5779c0b72258b_640x.png?v=1654355532" alt="Bot 1" class="bot-image">
         <div class="ms-2 me-auto">
           <div class="fw-bold">Limule Tempest</div>
-          Déscription du bot 1
+          Description du bot 1
         </div>
       </li>
       <li class="list-group-item d-flex justify-content-between align-items-start bot-item">
         <img src="https://i.pinimg.com/736x/d3/01/b6/d301b6d9ee94cee3ce81bb3352c049af.jpg" alt="Bot 2" class="bot-image">
         <div class="ms-2 me-auto">
           <div class="fw-bold">Veldra Tempest</div>
-          Déscription du bot 2
+          Description du bot 2
         </div>
       </li>
       <li class="list-group-item d-flex justify-content-between align-items-start bot-item">
         <img src="https://www.nautiljon.com/images/perso/00/85/benimaru_17758.webp" alt="Bot 3" class="bot-image">
         <div class="ms-2 me-auto">
           <div class="fw-bold">Benimaru</div>
-          Déscription du bot 3
+          Description du bot 3
         </div>
       </li>
       <li class="list-group-item d-flex justify-content-between align-items-start bot-item">
         <img src="https://i.pinimg.com/originals/72/48/30/7248304fefb72d4bfc0e5daa2109da1c.jpg" alt="Bot 3" class="bot-image">
         <div class="ms-2 me-auto">
           <div class="fw-bold">Diablo</div>
-          Déscription du bot 3
+          Description du bot 3
         </div>
       </li>
     </ol>
