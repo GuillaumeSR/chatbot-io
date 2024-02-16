76535e0a7196d52d19a1df69a7bc823b7f7bd2a8
Maxime <maxime.lecourt457@gmail.com>
2024-02-16 14:46:35 +0100

    htmlv2

--- src/views/nav.js
+++ src/views/nav.js
@@ -1,10 +1,13 @@
 export default () => (`
   <nav class="navbar bg-body-tertiary">
-  <form class="container-fluid justify-content-start">
-    <div class="btn-nav">
-      <button class="btn-unique"type="button">All Bots</button>
-      <button class="btn-unique" type="button">Chat with bots</button>
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+      <div>
+        <img class="logo" src="https://cdn.discordapp.com/attachments/1160934866527981649/1208049234813784125/bulbi-removebg-preview.png?ex=65e1de86&is=65cf6986&hm=8e62327ff9d9aa036c1911e0ffea2e031b654277a7ab6adc999218d8e475995c&" alt="Logo">
+      </div>
+      <form class="btn-nav">
+        <button class="btn-unique" type="button">All Bots</button>
+        <button class="btn-unique" type="button">Chat with bots</button>
+      </form>
     </div>
-  </form>
-</nav>
+  </nav>
 `);
