3aa35dbb873000c9a12e19f4328d3f554ee250b1
Maxime <maxime.lecourt457@gmail.com>
2024-02-22 16:02:23 +0100

    viewbotcard

---
+++ src/views/botcard.js
@@ -0,0 +1,19 @@
+export default (data) => {
+  const {
+    name,
+    image,
+    description
+  } = data;
+
+  return `
+  <div class="col-3">
+    <div class="card" style="width: 18rem;">
+      <img src=${image} class="card-img-top" alt="...">
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text">${description}</p>
+    </div>
+  </div>
+  `;
+};
