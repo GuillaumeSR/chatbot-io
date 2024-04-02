5361ef5fba1a2150f6ff18088b9b83c9aacba78f
Maxime <maxime.lecourt457@gmail.com>
2024-04-02 12:08:19 +0200

    add date

--- src/views/message.js
+++ src/views/message.js
@@ -5,9 +5,12 @@ export default (data) => {
   } = data;
 
   return `
-    <div class="message ${userType}">
+  <div class="message ${userType}">
+    <div class="contentdate">
       <img src="https://i.pinimg.com/736x/d3/01/b6/d301b6d9ee94cee3ce81bb3352c049af.jpg" alt="User 2" class="bot-image">
-      <p>${content}</p>
+      <p> 12/06/23 </p>
     </div>
+    <p>${content}</p>      
+  </div>
   `;
 };
